c7e356494f504b1b2bb1c9a975376a762e16b0d4
maninmiddle <maninmiddler@gmail.com>
2023-11-29 05:39:55 +0300

    ignoring symbols

--- app/src/main/java/com/example/myapp/data/CipherRepositoryImpl.kt
+++ app/src/main/java/com/example/myapp/data/CipherRepositoryImpl.kt
@@ -5,6 +5,8 @@ import javax.inject.Inject
 
 class CipherRepositoryImpl @Inject constructor() : CipherRepository {
 
+    private val regexIgnoreSymbols = """[^A-Za-z0-9]""".toRegex()
+
     override fun generateAlphabet(lang: Enum<LangEnum>): List<Char> {
         var alphabet = mutableListOf<Char>()
         when (lang) {
@@ -30,14 +32,14 @@ class CipherRepositoryImpl @Inject constructor() : CipherRepository {
         var res = ""
 
         for (i in cipherModel.text.uppercase().toList()) {
-            res += if (i != ' ') {
+            res += if (regexIgnoreSymbols.matchEntire(i.toString()) == null) {
                 if (alphabet.indexOf(i) < cipherModel.shift) {
                     alphabet.last() + alphabet.indexOf(i) - cipherModel.shift + 1
                 } else {
                     alphabet[alphabet.indexOf(i) - cipherModel.shift]
                 }
             } else {
-                ' '
+                i
             }
         }
         return res.lowercase()
@@ -48,14 +50,14 @@ class CipherRepositoryImpl @Inject constructor() : CipherRepository {
         var res = ""
         val lastCharPosition = alphabet.indexOf(alphabet.last()) + 1
         for (i in cipherModel.text.uppercase().toList()) {
-            res += if (i != ' ') {
+            res += if (regexIgnoreSymbols.matchEntire(i.toString()) == null) {
                 if (alphabet.indexOf(i) + cipherModel.shift > lastCharPosition) {
                     alphabet.first() + (alphabet.indexOf(i) + cipherModel.shift - lastCharPosition)
                 } else {
                     alphabet[alphabet.indexOf(i) + cipherModel.shift]
                 }
             } else {
-                ' '
+                i
             }
         }
         return res.lowercase()
